91bf56a216ffba0b549ddeea4830b88034340a9b
mjkasper <mkasper@andrew.cmu.edu>
2018-06-24 00:24:47 -0500

    improved setup script

---
+++ settings.sh
@@ -0,0 +1,23 @@
+# This is the default path where any binaries (such as NeoVim) will be
+# installed on the new machine.
+#
+# Note: Though this could likely default to something like /usr/local/bin
+# or some location like that, I frequenly find myself having to port my
+# dev setup to machines where I do not have root access (i.e. shared clusters
+# that I may use for work or school). In this case, I find it much easier to
+# just keep a private $(HOME)/bin directory, which I can add to my system
+# path, that contains all of my custom installed binaries.
+DEFAULT_BIN_PATH=~/bin
+
+# If set to true, the install.sh script will cause NeoVim to be installed
+# in the DEFAULT_BIN_PATH directory. It will also install Plug as a plugin
+# manager, and import basic NeoVim configurations.
+INSTALL_NEOVIM=true
+
+# If set to true, the install.sh script will create a ~/.bashrc containing
+# the contents of the ./bashrc file in this directory.
+INSTALL_BASHRC=true
+
+# If set to true, the install.sh script will create a ~/.bash_profile containing
+# the contents of the ./bash_profile file in this directory.
+INSTALL_BASH_PROFILE=true
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# default path for binaries to be installed (change this as needed)
-DEFAULT_BIN_PATH=~/bin
+source ./settings.sh
 
 # Create default folder to store binaries if needed.
 #
@@ -16,28 +15,47 @@ fi
 #                               Install NeoVim                               #
 ##############################################################################
 
-# download the neovim binary and save it as $(DEFAULT_BIN_PATH)/neovim
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x neovim.appimage
-mv neovim.appimage "$DEFAULT_BIN_PATH"/neovim
+if [ "$INSTALL_NEOVIM" = true ]; then
+    # download the neovim binary and save it as $(DEFAULT_BIN_PATH)/neovim
+    curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+    chmod u+x neovim.appimage
+    mv neovim.appimage "$DEFAULT_BIN_PATH"/nvim
 
-# create a ~/.config directory if needed
-if [ ! -d ~/.config ]; then
-    mkdir ~/.config
+    # add the binary directory to our PATH variable
+    echo "export \"PATH=${DEFAULT_BIN_PATH}:$PATH\"" >> ~/.bashrc
+
+    # download the Plug package manager for NeoVim
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    # create a ~/.config directory if needed
+    if [ ! -d ~/.config ]; then
+        mkdir ~/.config
+    fi
+
+    # create a ~/.config/nvim directory if needed
+    if [ ! -d ~/.config/nvim ]; then
+        mkdir ~/.config/nvim
+    fi
+
+    # copy our neovim settings into init.vim
+    cat init.vim > ~/.config/nvim/init.vim
 fi
-
-# create a ~/.config/nvim directory if needed
-if [ ! -d ~/.config/nvim ]; then
-    mkdir ~/.config/nvim
-fi
-
-# copy our neovim settings into init.vim
-cat init.vim > ~/.config/nvim/init.vim
 
 ##############################################################################
 #                           Install Bash Settings                            #
 ##############################################################################
 
-cat ./bashrc > ~/.bashrc
-cat ./bash_profile > ~/.bash_profile
+# if requested, include a new ~/.bashrc
+if [ "$INSTALL_BASHRC" = true ]; then
+    cat ./bashrc > ~/.bashrc
+fi
+
+# if requested, include a new ~/.bash_profile
+if [ "$INSTALL_BASH_PROFILE" = true ]; then
+    cat ./bash_profile > ~/.bash_profile
+fi
+
+# run the ~/.bashrc to reflect any new changes
+source ~/.bashrc
 
